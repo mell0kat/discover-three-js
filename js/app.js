1004cd97e131e1d4014bd72ad95c17b119ac6426
Katherine Mello <katherine.mello8@gmail.com>
2020-07-22 09:16:40 -0400

    Handles browser resizing & refactors code more

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ const init = () => {
 
   scene = new THREE.Scene()
 
-  scene.background = new THREE.Color('skyblue')
+  scene.background = new THREE.Color(0x567528)
 
   const FOV = 35
   const aspectRatio = container.clientWidth / container.clientHeight
@@ -22,7 +22,8 @@ const init = () => {
   const geometry = new THREE.BoxBufferGeometry(2, 2, 2)
 
   // Mesh Basics completely ignore light
-  const material = new THREE.MeshBasicMaterial({ color: 0x800080 })
+  // whereas Mesh Standard is "physically correct", but lower performance
+  const material = new THREE.MeshStandardMaterial({ color: 0x9d3750 })
 
   // Meshes are the most common kind of visible object in 3D computer graphics
   mesh = new THREE.Mesh(geometry, material)
@@ -30,7 +31,11 @@ const init = () => {
   scene.add(mesh)
   // could do scene.remove() if we needed to
 
-  renderer = new THREE.WebGLRenderer()
+  const light = new THREE.DirectionalLight(0xffffff, 4.0)
+  light.position.set(10, 10, 10)
+  scene.add(light)
+
+  renderer = new THREE.WebGLRenderer({ antialias: true })
 
   renderer.setSize(container.clientWidth, container.clientHeight)
   // this helps with mobile responsiveness
@@ -39,10 +44,36 @@ const init = () => {
   container.appendChild(renderer.domElement)
 }
 
-const animate = () => {
-  requestAnimationFrame(animate)
+const update = () => {
+  mesh.rotation.x += 0.01
+  mesh.rotation.y += 0.01
+  mesh.rotation.z += 0.01
+}
+
+const render = () => {
   renderer.render(scene, camera)
 }
 
+const onWindowResize = () => {
+  camera.aspect = container.clientWidth / container.clientHeight
+
+  // update camera's frustum
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(container.clientWidth, container.clientHeight)
+}
+
+window.addEventListener('resize', onWindowResize)
+/* The game(animation) loop:
+  Get user input
+  Update animations
+  Render the frame
+*/
 init()
-animate()
+
+// setAnimationLoop abstracts away the logic of requestAnimationFrame, which behaves differently in WebXR
+renderer.setAnimationLoop(() => {
+  render()
+  update()
+})
+// if we need to cancel animation, we can so renderer.setAnimationLoop(null);
